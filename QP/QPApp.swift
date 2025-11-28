1dc09e852f5f862adc5e9e3d2df9e1706dd69a22
Vijaymadhav Chittipeddi <vijay.madhav@gmail.com>
2025-11-28 15:36:09 -0800

    Add cached image loader and URL cache tuning

--- QP/QPApp.swift
+++ QP/QPApp.swift
@@ -12,11 +12,22 @@ struct QPApp: App {
     @StateObject private var appState = AppState()
     let persistenceController = PersistenceController.shared
 
+    init() {
+        configureURLCache()
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(appState)
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+    
+    private func configureURLCache() {
+        let memory = 60 * 1024 * 1024
+        let disk = 250 * 1024 * 1024
+        URLCache.shared.memoryCapacity = memory
+        URLCache.shared.diskCapacity = disk
     }
 }
